7b8d278a83c928da20b99c145b8cc220372289a0
Paul Dilley <pldilley@gmail.com>
2020-05-02 21:39:00 -0400

    Re-add build

---
+++ src/chat-app/build/precache-manifest.69392b53f05e48e919504ae981d60f11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41780782e93bc848264cd081e81140d4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "723400528f0e65ab5ac0",
+    "url": "./static/css/main.80f2401f.chunk.css"
+  },
+  {
+    "revision": "1b30f619a0891fde97d3",
+    "url": "./static/js/2.572f4a4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.572f4a4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723400528f0e65ab5ac0",
+    "url": "./static/js/main.e7a7121c.chunk.js"
+  },
+  {
+    "revision": "a68e76a8c58717f1e2b3",
+    "url": "./static/js/runtime-main.643e5758.js"
+  }
+]);
